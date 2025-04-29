f6e772c5aad1a45c773b15c6d7a4c76ae1e57951
Rachit <rachitjain569@gmail.com>
2025-04-30 03:54:45 +0530

    minor changes

--- src/components/section/dashBoardBottom.js
+++ src/components/section/dashBoardBottom.js
@@ -2,11 +2,15 @@ import PendingQuestions from "../cards/pendingQuestion";
 import ChatbotWelcome from "../cards/aiChatBot";
 export default function DashBoardBottom() {
     return (
-        <div className='flex flex-row '>
-            {/* This is the left pending question section*/}
-            <PendingQuestions />
-            {/* This is the right chatbot section*/}
-            <ChatbotWelcome />
+        <div className="flex flex-col lg:flex-row w-full gap-4">
+            {/* This is the left pending question section */}
+            <div className="flex-1">
+                <PendingQuestions />
+            </div>
+            {/* This is the right chatbot section */}
+            <div className="flex-1">
+                <ChatbotWelcome />
+            </div>
         </div>
     );
 }
